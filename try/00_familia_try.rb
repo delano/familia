4c38f65c5ae1496fa541aaeb6cd0cce760bde0c0
delano <delano@cpan.org>
2024-06-16 15:30:45 -0700

    [NOJIRA] Improve Familia try script readability
    Re-enable 00_familia_try.rb tests. Tests were added to check Familia's
    Redis connection, URI handling, and that URL is an alias of URI.

---
+++ try/00_familia_try.rb
@@ -0,0 +1,21 @@
+require 'familia'
+require 'familia/test_helpers'
+
+Familia.apiversion = 'v1'
+
+
+## Check for help class
+Bone.redis_objects.keys # consistent b/c hashes are ordered
+#=> [:owners, :tags, :metrics, :props, :value]
+
+## Familia has a uri
+Familia.uri.class
+#=> URI::Redis
+
+## Familia has a uri as a string
+Familia.uri.to_s
+#=> 'redis://127.0.0.1'
+
+## Familia has a url, an alias to uri
+Familia.url.eql?(Familia.uri)
+#=> true
