d7bec17e666bf954937096fc2d9ff9628360a0d5
delano <delano@onetimesecret.com>
2025-09-07 17:29:48 -0700

    [#92] Add tryouts tests for hot reload functionality
    Tests cover:
    - unload_member method removes specific class constants
    - unload! method removes all tracked member classes
    - Proper handling of class reloading scenarios
    - String/symbol parameter support

---
+++ try/core/hot_reload_try.rb
@@ -0,0 +1,84 @@
+require_relative '../helpers/test_helpers'
+require 'tmpdir'
+
+@tempdir = Dir.mktmpdir('familia_reload_test')
+@test_file = File.join(@tempdir, 'test_reloadable_class.rb')
+@test_class_name = 'TestReloadableClass'
+
+File.write(@test_file, <<~RUBY)
+  class #{@test_class_name} < Familia::Horreum
+    field :name
+    field :value
+  end
+RUBY
+
+class ::TestReloadableClassA < Familia::Horreum
+  field :data
+end
+
+class ::TestReloadableClassB < Familia::Horreum
+  field :info
+end
+
+class ::TestStringSymbol < Familia::Horreum
+  field :data_field
+end
+
+## Load a test class and add it to Familia.members for tracking
+load @test_file
+Familia.instance_variable_get(:@members) << @test_class_name
+TestReloadableClass.new(name: "test", value: "initial").class.name
+#=> "TestReloadableClass"
+
+## unload_member removes a specific class constant
+Familia.unload_member(@test_class_name)
+begin
+  TestReloadableClass
+rescue NameError => e
+  e.class.name
+end
+#=> "NameError"
+
+## Reload the class after unloading
+load @test_file
+TestReloadableClass.new(name: "test", value: "reloaded").class.name
+#=> "TestReloadableClass"
+
+## Set up member tracking for batch unloading test
+Familia.instance_variable_set(:@members, ['TestReloadableClassA', 'TestReloadableClassB'])
+Familia.instance_variable_get(:@members)
+#=> ["TestReloadableClassA", "TestReloadableClassB"]
+
+## unload! removes all tracked member classes
+Familia.unload!
+Familia.instance_variable_get(:@members)
+#=> ["TestReloadableClassA", "TestReloadableClassB"]
+
+## Verify both classes were unloaded
+errors = []
+begin
+  TestReloadableClassA
+rescue NameError
+  errors << "A"
+end
+
+begin
+  TestReloadableClassB
+rescue NameError
+  errors << "B"
+end
+
+errors.sort
+#=> ["A", "B"]
+
+## unload_member handles string and symbol inputs
+Familia.unload_member("TestStringSymbol")
+begin
+  TestStringSymbol
+rescue NameError => e
+  e.message.include?("TestStringSymbol")
+end
+#=> true
+
+# Cleanup test directory
+FileUtils.rm_rf(@tempdir)
